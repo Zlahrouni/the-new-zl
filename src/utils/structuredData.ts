5b055c912a0a90ab87b2bff007154c88f4c62c30
= <=>
2024-12-30 21:15:44 +0100

    update structured data for google

--- src/utils/structuredData.ts
+++ src/utils/structuredData.ts
@@ -1,41 +1,27 @@
 export const getStructuredData = (language: 'en' | 'fr') => ({
-    "@context": "http://schema.org",
+    "@context": "https://schema.org",
+    "@type": "ProfilePage",
+    "mainEntity": {
     "@type": "Person",
-    "name": "Ziad Lahrouni",
-    "jobTitle": language === 'en' ? "Full Stack Developer" : "Développeur Full Stack",
-    "url": "https://ziadlahrouni.com",
-    "sameAs": [
+        "name": "Ziad Lahrouni",
+        "jobTitle": language === 'en' ? "Full Stack Developer" : "Développeur Full Stack",
+        "url": "https://ziadlahrouni.com",
+        "sameAs": [
         "https://github.com/zlahrouni",
         "https://linkedin.com/in/ziad-lahrouni/"
     ],
-    "knowsAbout": [
-        "Angular",
-        "Spring Boot",
-        "React",
-        "TypeScript",
-        "Java",
-        "Full Stack Development"
-    ],
-    "worksFor": {
+        "worksFor": {
         "@type": "Organization",
-        "name": "CDC Informatique"
+            "name": "CDC Informatique"
     },
-    "alumniOf": [
-        {
-            "@type": "CollegeOrUniversity",
-            "name": language === "en" ? "University of Western Brittany" : "Université de Bretagne Occidentale",
-            "alternateName": "UBO"
-        }
-    ],
     "attends": {
         "@type": "CollegeOrUniversity",
-        "name": "Efrei Paris",
-        "programName": language === "en" ? "Full Stack Dev Manager - Apprenticeship" : "Développeur Manager Full Stack - Apprentissage"
+            "name": "Efrei Paris"
     },
-    "skills": [
-        "Web Development",
-        "Software Architecture",
-        "DevOps",
-        "Agile Methodologies"
-    ]
+    "alumniOf": {
+        "@type": "CollegeOrUniversity",
+            "name": "University of Western Brittany",
+            "alternateName": "UBO"
+    }
+}
 });
